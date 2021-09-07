4f7f0b9f4e8accfd29aa506494c18975553f9400
JoseVel4zquez <76269358+JoseVel4zquez@users.noreply.github.com>
2021-09-07 16:03:15 -0500

    log de advertencias

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import React, {useState, useEffect} from "react";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import React, { useState, useEffect } from "react";
+import { LogBox } from "react-native";
 import Navigation from "./App/navigation/Navigation";
 
+LogBox.ignoreLogs([
+  "Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.",
+]);
+
 export default function App() {
-  return(
-    <Navigation />
-  )
+  return <Navigation />;
 }
